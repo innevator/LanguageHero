a16cc68311d7b7c5ea49c93a3d6af810d27fd820
Evan <innevatorg@gmail.com>
2023-11-29 11:57:08 +0800

    create framework project

---
+++ LanguageHeroFeature/LanguageHeroFeature/LanguageHeroFeature.h
@@ -0,0 +1,18 @@
+//
+//  LanguageHeroFeature.h
+//  LanguageHeroFeature
+//
+//  Created by 洪宗鴻 on 2023/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LanguageHeroFeature.
+FOUNDATION_EXPORT double LanguageHeroFeatureVersionNumber;
+
+//! Project version string for LanguageHeroFeature.
+FOUNDATION_EXPORT const unsigned char LanguageHeroFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanguageHeroFeature/PublicHeader.h>
+
+
