32ebf46e17834dc296c2b659e7d72239455a72be
Evan <innevatorg@gmail.com>
2023-12-02 02:07:59 +0800

    implement simple gameView

--- LanguageHeroFeature/LanguageHeroApp/ContentView.swift
+++ LanguageHeroFeature/LanguageHeroApp/ContentView.swift
@@ -8,28 +8,74 @@
 import SwiftUI
 import LanguageHeroFeature
 
-let talks: [String] = ["talk1", "talk2"]
+let talks: [String] = ["fireball", "thunder"]
 let defaultTalks: [Talk] = talks.map { Talk(value: $0) }
 
 struct ContentView: View {
-    @StateObject var gameProcessor = GameProcessor(talks: defaultTalks)
+    @StateObject var gameProcessor = GameProcessor(talks: defaultTalks, hero: Hero(), monster: Monster())
     
     var body: some View {
         VStack {
             Text("分數: \(gameProcessor.score)")
             
-            Text(gameProcessor.currentTalk?.value ?? "")
+            if gameProcessor.isOver {
+                Spacer()
+                
+                restartView
+            }
+            else {
+                monsterView
+                
+                Spacer()
+                
+                talkView
+                
+                Spacer()
+                
+                heroView
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+    
+    var monsterView: some View {
+        VStack {
+            ProgressView(value: Double(gameProcessor.attackingMonster.hp), total: Double(gameProcessor.monster.hp)).tint(Color.red)
+            
+            Rectangle().fill(Color.black).frame(width: 100, height: 100)
+            
+            Text("MonsterName")
+        }
+    }
+    
+    var restartView: some View {
+        Button("restart") {
+            gameProcessor.restart()
+        }
+    }
+    
+    var talkView: some View {
+        Text(gameProcessor.currentTalk?.value ?? "")
+    }
+    
+    var heroView: some View {
+        VStack {
+            Rectangle().fill(Color.black).frame(width: 100, height: 100)
+            
+            Text("HeroName")
+            ProgressView(value: Double(gameProcessor.hero.hp), total: Double(gameProcessor.hero.hp)).tint(Color.red)
             
             HStack {
                 Button("action1") {
-                    gameProcessor.execute(input: "talk1")
+                    gameProcessor.execute(input: defaultTalks[0].value)
                 }
                 Button("action2") {
-                    gameProcessor.execute(input: "talk2")
+                    gameProcessor.execute(input: defaultTalks[1].value)
                 }
             }
         }
-        .padding()
     }
 }
 
