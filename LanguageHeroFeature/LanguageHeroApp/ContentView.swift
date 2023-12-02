ff63d8e8e482841b46330547d5123ac22eea9b7f
Evan <innevatorg@gmail.com>
2023-12-03 00:29:11 +0800

    implement gameView countdoun timer

--- LanguageHeroFeature/LanguageHeroApp/ContentView.swift
+++ LanguageHeroFeature/LanguageHeroApp/ContentView.swift
@@ -13,9 +13,12 @@ let defaultTalks: [Talk] = talks.map { Talk(value: $0) }
 
 struct ContentView: View {
     @StateObject var gameProcessor = GameProcessor(talks: defaultTalks, hero: Hero(), monster: Monster())
+    @State var currentSeconds: Int = 0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack {
+            Text("時間: \(currentSeconds)")
             Text("分數: \(gameProcessor.score)")
             
             if gameProcessor.isOver {
@@ -38,11 +41,16 @@ struct ContentView: View {
             Spacer()
         }
         .padding()
+        .onReceive(timer) { _ in
+            if !gameProcessor.isOver {
+                currentSeconds += 1
+            }
+        }
     }
     
     var monsterView: some View {
         VStack {
-            ProgressView(value: Double(gameProcessor.attackingMonster.hp), total: Double(gameProcessor.monster.hp)).tint(Color.red)
+            ProgressView(value: Double(gameProcessor.monster.hp), total: Double(gameProcessor.monster.maxHp)).tint(Color.red)
             
             Rectangle().fill(Color.black).frame(width: 100, height: 100)
             
@@ -53,6 +61,7 @@ struct ContentView: View {
     var restartView: some View {
         Button("restart") {
             gameProcessor.restart()
+            currentSeconds = 0
         }
     }
     
@@ -65,7 +74,7 @@ struct ContentView: View {
             Rectangle().fill(Color.black).frame(width: 100, height: 100)
             
             Text("HeroName")
-            ProgressView(value: Double(gameProcessor.hero.hp), total: Double(gameProcessor.hero.hp)).tint(Color.red)
+            ProgressView(value: Double(gameProcessor.hero.hp), total: Double(gameProcessor.hero.maxHp)).tint(Color.red)
             
             HStack {
                 Button("action1") {
